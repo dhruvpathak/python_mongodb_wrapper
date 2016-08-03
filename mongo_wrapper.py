b4484ebb50dc61267e583d7f77d6d15304ea0ea5
dhruvpathak <getdhruvpathak@gmail.com>
2016-08-03 18:17:03 +0530

    read preference as object parameter

--- mongo_wrapper.py
+++ mongo_wrapper.py
@@ -1,8 +1,8 @@
-__author__ = "Dhruv Pathak"
+__author__ = 'Dhruv Pathak'
 
 from django.conf import settings
+import pymongo
 from pymongo.read_preferences import ReadPreference
-from pymongo import MongoClient
 
 
 class MongoWrapper(object):
@@ -12,28 +12,24 @@ class MongoWrapper(object):
     def __init__(self):
         self.__dict__ = self._shared_state
 
+  
+    def mongo_db_connect(self, server_key, database, read_preference='PRIMARY_PREFERRED'):
+        if self.db_conn_map.get(server_key, {}).get(database, {}).get(read_preference,None) is not None:
+ 	        return self.db_conn_map[server_key][database][read_preference]
 
-    def mongo_db_connect(self,server_key,database):
-        if self.db_conn_map.get(server_key, {}).get(database, None) is not None:
-            return self.db_conn_map[server_key][database]
+        mongo_server_conf = settings.MONGODB_SERVER_CONF['servers'][server_key]
+        connection_read_preference = getattr(ReadPreference,read_preference)
 
-        mongo_server_conf = settings.MONGODB_COMBINED_STORAGE_CONF['server_sets'][server_key]
         if mongo_server_conf.get('REPLICA_SET_NAME') is None:
-            mongo_conn = MongoClient(host=mongo_server_conf['HOST'], read_preference=ReadPreference.PRIMARY_PREFERRED)
+            mongo_conn = pymongo.MongoClient(host=mongo_server_conf['HOST'], read_preference=connection_read_preference, w= 0)
         else:
-            mongo_conn = MongoClient(host=mongo_server_conf['HOST'], replicaset=mongo_server_conf['REPLICA_SET_NAME'], read_preference=ReadPreference.PRIMARY_PREFERRED)
+            mongo_conn = pymongo.MongoReplicaSetClient(host=mongo_server_conf['HOST'], read_preference=connection_read_preference, w=0, replicaSet=mongo_server_conf['REPLICA_SET_NAME'])
 
-        if database not in settings.MONGODB_COMBINED_STORAGE_CONF['db_collections']:
-            raise Exception(u"'{0}' is not a predefined database".format(database))
+        mongo_db = mongo_conn[settings.MONGODB_STORAGE_CONF['databases'][database]]
+        self.db_conn_map.setdefault(server_key, {}).setdefault(database,{})[read_preference] = mongo_db
 
-        mongo_db = mongo_conn[database]
-        self.db_conn_map.setdefault(server_key, {})[database] = mongo_db
         return mongo_db
 
-    def get_mongo_collection(self, collection_name, database, server_key):
+    def get_mongo_collection(self, collection_name, database, server_key ):
         mongo_db = self.mongo_db_connect(server_key, database)
-        if collection_name not in settings.MONGODB_COMBINED_STORAGE_CONF['db_collections'][database]:
-            raise Exception(u"'{0}' is not a predefined collection in '{1}' database".format(collection_name,database))
-        return mongo_db[collection_name]
-
-
+        return mongo_db[settings.MONGODB_STORAGE_CONF['collections'][collection_name]]
